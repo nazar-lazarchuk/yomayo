5c2911807c197ecad0d4c900cc3c0b6e8eaeb6d2
NazarLazarchuk <nazik.lazarchyk@gmail.com>
2020-07-29 12:48:16 +0300

    lib refactoring, actions event handler

--- example/components/MyTestComponent/actions.js
+++ example/components/MyTestComponent/actions.js
@@ -1,13 +1,16 @@
 // функції можуть змінювати state та ловити computed/props через контекст this
 
-export const doIncrement = () => {
+export const doIncrement = function() {
   this.count++;
+  console.log('doIncrement');
+  return true;
 }
 
-export const doDecrement = () => {
+export const doDecrement = function() {
   this.count--;
+  console.log('doDecrement');
 }
 
-export const showProp1 = () => {
+export const showProp1 = function() {
   alert(this.myProp1);
 }
