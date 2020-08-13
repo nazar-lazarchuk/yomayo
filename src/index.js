c2df0d0555dac58370b8434ccb82b9fcb3a3111c
NazarLazarchuk <nazik.lazarchyk@gmail.com>
2020-08-13 21:06:27 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,69 +1,16 @@
+import r from './renderApp';
 import createState from './createState';
 import mount from './mount';
 import update from './update';
 
-/**
- * Кореневий вузол для апки
- * @type {HTMLElement}
- */
-let rootNode = null;
+function renderApp (...args) {
+  const lifecycleMethods = {
+    createState,
+    mount,
+    update,
+  };
 
-function renderApp(selector, rootComponent) {
-  if (rootNode) {
-    return;
-  }
-
-  rootNode = document.querySelector(selector);
-
-  // Розпаковуємо комопонент
-  // TODO: перевірка на відсутність чого-небудь із даних
-  const {
-    state: { initialState, computedState },
-    actions,
-  } = rootComponent;
-  const getVirtualDOM = rootComponent.render;
-
-  const state = createState(initialState, computedState, handleUpdate);
-
-  const bindedActions = getBindedActions(actions, state);
-
-  const initialVDOM = getVirtualDOM({ ...state, ...bindedActions });
-
-  /**
-   * Змінна для актуального Virtual DOM
-   * @type {Object}
-   */
-  let VDOM = initialVDOM;
-
-  // генерація DOM в перший раз
-  mount(rootNode, VDOM);
-
-  console.log(state, VDOM);
-
-  function handleUpdate() {
-    const newVDOM = getVirtualDOM({ ...state, ...bindedActions });
-    update(newVDOM, VDOM);
-  
-    VDOM = newVDOM;
-  
-    console.log('Змінився стан компонента!', VDOM);
-  }
-}
-
-/**
- * Функція для привязки методів до контексту компонента
- * @param {Object} actions події
- * @param {ProxyConstructor} state стан компоненту
- * @returns {Object} 
- */
-function getBindedActions(actions, state) {
-  const result = {};
-
-  Object.keys(actions).forEach(key => {
-    result[key] = event => actions[key].call(state, event);
-  });
-
-  return result;
+  return r(lifecycleMethods, ...args);
 }
 
 function createElement(tag, props, ...children) {
